1bf00a0a58771d806bd7b02247f6a7b22364acb4
kromanidis <k.romanidis@blockachain.gr>
2020-02-28 13:19:36 +0200

    migrate to kotlin-dsl

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+object ApplicationId {
+    const val id = "com.kroman.hiitimer"
+}
+
+object Releases {
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
+
+object Versions {
+    const val kotlin = "1.3.50"
+}
+
+object Libraries {
+
+}
+
+object TestLibraries {
+
+}
