bb48da3f2dc51ae48be03c86990f3465567c79cd
kromanidis <k.romanidis@blockachain.gr>
2021-10-16 23:25:04 +0300

    fix viewmodel import for viewmodels module di

--- presentation/src/main/kotlin/com/kroman/presentation/di/ViewModelsModule.kt
+++ presentation/src/main/kotlin/com/kroman/presentation/di/ViewModelsModule.kt
@@ -1,7 +1,7 @@
 package com.kroman.presentation.di
 
 import com.kroman.presentation.ui.main.MainViewModel
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
